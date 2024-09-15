5138c2c0471e28f9113620609fb72f0a92633a0e
Marko Nikolaš <36996299+markonikolas@users.noreply.github.com>
2024-09-15 13:50:13 +0200

    Add starter block

--- src/edit.js
+++ src/edit.js
@@ -1,15 +1,45 @@
+import PropTypes from 'prop-types';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
 
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const { kicker, headline, subdeck } = attributes;
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Funky Headline – hello from the editor!',
-				'funky-headline'
-			) }
-		</p>
+		<div { ...useBlockProps() }>
+			<RichText
+				tagName="span"
+				value={ kicker }
+				onChange={ ( value ) => setAttributes( { kicker: value } ) }
+				placeholder={ __( 'Awesome Kicker…' ) }
+				className="wp-block__kicker"
+				allowedFormats={ [ 'core/bold', 'core/italic' ] }
+			/>
+			<RichText
+				tagName="h1"
+				value={ headline }
+				onChange={ ( value ) => setAttributes( { headline: value } ) }
+				placeholder={ __( 'Awesome Headline…' ) }
+				className="wp-block__headline"
+				allowedFormats={ [ 'core/bold', 'core/italic' ] }
+			/>
+			<RichText
+				tagName="p"
+				value={ subdeck }
+				onChange={ ( value ) => setAttributes( { subdeck: value } ) }
+				placeholder={ __( 'Awesome Subdeck…' ) }
+				className="wp-block__subdeck"
+			/>
+		</div>
 	);
 }
+
+Edit.propTypes = {
+	attributes: PropTypes.object,
+	setAttributes: PropTypes.func,
+	kicker: PropTypes.string,
+	headline: PropTypes.string,
+	subdeck: PropTypes.string,
+};
--- src/save.js
+++ src/save.js
@@ -1,9 +1,32 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import PropTypes from 'prop-types';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
 
-export default function Save() {
+export default function Save( { attributes } ) {
+	const { Content } = RichText;
+	const { kicker, headline, subdeck } = attributes;
+
 	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Funky Headline – hello from the saved content!' }
-		</p>
+		<div { ...useBlockProps.save() }>
+			<Content
+				tagName="span"
+				value={ kicker }
+			/>
+			<Content
+				tagName="h2"
+				value={ headline }
+			/>
+			<Content
+				tagName="p"
+				value={ subdeck }
+			/>
+
+		</div>
 	);
+}
+
+Save.PropTypes = {
+	attributes: PropTypes.object,
+	kicker: PropTypes.string,
+	headline: PropTypes.string,
+	subdeck: PropTypes.string,
 }
